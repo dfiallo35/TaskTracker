91b16673277c3284135c6e817f9c0e986d90d5b3
Dennis Fiallo <dfiallo35@gmail.com>
2024-07-31 22:43:00 -0400

    update task-input and title

--- frontend/src/app/components/task-list/task-list.component.ts
+++ frontend/src/app/components/task-list/task-list.component.ts
@@ -3,17 +3,22 @@ import { Task } from '../../services/task';
 import { TaskComponent } from "../task/task.component";
 import { CommonModule } from '@angular/common';
 import { TaskService } from '../../services/task.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [TaskComponent, CommonModule],
+  imports: [TaskComponent, CommonModule, FontAwesomeModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
   taskService: TaskService = inject(TaskService);
   taskList: Task[] = [];
+
+  // ICONS
+  faPlus = faPlus;
 
   constructor() {
     this.taskList = this.taskService.getAllTasks();
