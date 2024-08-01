23a5e6a747223c22d3069e5bbc5dca9d29e79d23
lauolivia <guerralauren44@gmail.com>
2024-07-31 22:54:54 -0400

    task-area

--- frontend/src/app/components/task/task.component.ts
+++ frontend/src/app/components/task/task.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Task } from '../../services/task';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   @Input() task!:Task;
+  faEdit = faEdit;
+  faTrash = faTrash;
+
 }
